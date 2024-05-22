b969ad312ec83d54fc8a8d7ab84f35ff857ddfb5
VictorFernandes2005 <fernandes.victor2708@gmail.com>
2024-05-22 12:12:57 -0300

    create/config DB connection

---
+++ src/main/java/com/estudo/TesteCrud/DataConfiguration.java
@@ -0,0 +1,37 @@
+package com.estudo.TesteCrud;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+// @configuration determina para o spring que essa é uma classe de configuração
+@Configuration
+public class DataConfiguration {
+    // @bean determina para o spring que essa classe precisará ser instaciada por ele
+    //(ele fica responsável por instaciar essa classe quando necessário)
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/bancodedados?useTimezone=true&serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("senha");
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter  = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MariaDBDialect");  
+        adapter.setPrepareConnection(true);      
+        return adapter;
+    }
+}
