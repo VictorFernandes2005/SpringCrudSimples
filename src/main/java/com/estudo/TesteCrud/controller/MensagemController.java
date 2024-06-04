7aa7e14c37f28b5ee4084d1767dffdde19dbd7b8
VictorFernandes2005 <fernandes.victor2708@gmail.com>
2024-06-04 15:55:18 -0300

    update Mensagem Model/Controller

--- src/main/java/com/estudo/TesteCrud/controller/MensagemController.java
+++ src/main/java/com/estudo/TesteCrud/controller/MensagemController.java
@@ -1,7 +1,10 @@
 package com.estudo.TesteCrud.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,30 +30,66 @@ public class MensagemController {
     }
 
     @RequestMapping(value="/novaMensagem", method=RequestMethod.GET) // redireciona para o msgAdd
-    public String getAddMsg(){
+    public String getAddMsg(Model model){
+        MensagemModel msg = new MensagemModel();
+        model.addAttribute("msg", msg);
         return "msg/msgAdd";
     }
 
     @RequestMapping(value="/novaMensagem",method=RequestMethod.POST) // cria uma nova mensagem
-    public String postAddMsg(@Valid MensagemModel msg, BindingResult result){
+    public String postAddMsg(@Valid MensagemModel msg, BindingResult result, Model model){
+        if(result.hasErrors()){
+            model.addAttribute("msg",msg);
+            model.addAttribute("error", "Erro! Um ou mais campos de textos inválidos!");
+            return "msg/msgAdd";
+        }
         mr.save(msg);
         return"redirect:/";
     }
 
     @RequestMapping(value="/editarMensagem/{id}",method=RequestMethod.GET) // redireciona para o msgEdit
     public ModelAndView getEditMsg(@PathVariable("id") long id){
-        System.out.println("*\nid: "+id+"\n*");
         ModelAndView mv = new ModelAndView("/msg/msgEdit");
-        mv.addObject("msg", mr.findById(id));
+        Optional<MensagemModel> msg = mr.findById(id);
+        if(!msg.isPresent()){
+            mv.addObject("error", "Não existe nenhuma mensagem com o id inserido!");
+            return mv;
+        }
+        mv.addObject("msg", msg);
         return mv;
     }
     @RequestMapping(value="/editarMensagem",method=RequestMethod.POST) // edita a mensagem
-    public String postEditMsg(@Valid MensagemModel msg, BindingResult result){
+    public String postEditMsg(@Valid MensagemModel msg, BindingResult result, Model model){
+        if(result.hasErrors()){
+            model.addAttribute("msg",msg);
+            model.addAttribute("error", "Erro! Um ou mais campos de textos inválidos!");
+            return "msg/msgEdit";
+        }
         MensagemModel bdMsg = mr.findById(msg.getId()).get();
         bdMsg.setMensagem(msg.getMensagem());
         bdMsg.setTitulo(msg.getTitulo());
         bdMsg.setData(msg.getData());
         mr.save(bdMsg);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value="/deletarMensagem/{id}",method=RequestMethod.GET) // redireciona para o msgDel
+    public ModelAndView getDelMsg(@PathVariable("id") long id){
+        ModelAndView mv = new ModelAndView("/msg/msgDel");
+        Optional<MensagemModel> msg = mr.findById(id);
+        if(!msg.isPresent()){
+            mv.addObject("error", "Não existe nenhuma mensagem com o id inserido!");
+            return mv;
+        }
+        mv.addObject("titulo", msg.get().getTitulo());
+        mv.addObject("mensagem", msg.get().getMensagem());
+        mv.addObject("data", msg.get().getData());
+        mv.addObject("msg", msg);
+        return mv;
+    }
+    @RequestMapping(value="/deletarMensagem",method=RequestMethod.POST) // deleta a mensagem
+    public String postDelMsg(@Valid MensagemModel msg){
+        mr.deleteById(msg.getId());
         return "redirect:/";
     }
 }
--- src/main/java/com/estudo/TesteCrud/model/MensagemModel.java
+++ src/main/java/com/estudo/TesteCrud/model/MensagemModel.java
@@ -1,12 +1,10 @@
 package com.estudo.TesteCrud.model;
 
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
